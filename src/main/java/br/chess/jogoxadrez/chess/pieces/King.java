0e7167b606ec9596725aa86b336d776c4038f752
lucas bueno <lucas-50p>
2020-05-08 13:22:07 -0300

    Movimentos do Rei

--- src/main/java/br/chess/jogoxadrez/chess/pieces/King.java
+++ src/main/java/br/chess/jogoxadrez/chess/pieces/King.java
@@ -1,26 +1,83 @@
 package br.chess.jogoxadrez.chess.pieces;
 
 import br.chess.jogoxadrez.boardgame.Board;
+import br.chess.jogoxadrez.boardgame.Position;
 import br.chess.jogoxadrez.chess.ChessPiece;
 import br.chess.jogoxadrez.chess.Color;
 
 /*Rei - Tabuleiro*/
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
-	/*Esse construtor vai repassar essa chamada para a super class*/
+	/* Esse construtor vai repassar essa chamada para a super class */
 	public King(Board board, Color color) {
 		super(board, color);
 	}
 
-	/* Essa letra no tabuleiro*/
+	/* Essa letra no tabuleiro */
 	@Override
 	public String toString() {
 		return "K";
 	}
 
+	/* Pode mover */
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// acima
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// baixo
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Esquerda
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Direita
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Noroeste
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Nordeste
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Sudoeste
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// Sudeste
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
 		return mat;
 	}
 }
--- src/main/java/br/chess/jogoxadrez/main/UI.java
+++ src/main/java/br/chess/jogoxadrez/main/UI.java
@@ -65,19 +65,19 @@ public class UI {
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print((8 - i) + "   ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println("  a  b  c  d e f g h");
 	}
 
 	/*Imprimir uma unica peça*/
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
-			System.out.print("*");
+			System.out.print("");
 		}
 		if (piece == null) {
 			System.out.print("-");
